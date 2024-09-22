d67baee89d8004ea2c226788e041cd5d368eccec
Farouk Adeleke <fadel@Farouks-MacBook-Air.local>
2024-09-22 12:45:05 -0400

    link styling

--- sections/Writing.tsx
+++ sections/Writing.tsx
@@ -20,7 +20,7 @@ function Writing() {
             }
             target="_blank"
           >
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight flex flex-inline items-center hover:underline">
+            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight flex flex-wrap lg:flex-inline items-center hover:underline">
               <Link2 className="mr-2" /> ai as a catalyst for a circular economy
             </h3>
           </Link>
